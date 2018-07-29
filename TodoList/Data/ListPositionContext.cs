2ec9b259d36cac8ea666ec52837880300987bcc7
Dudosn <wojciechduda4@gmail.com>
2018-07-29 19:08:13 +0200

    1. Added logic to communicate with InMemoryDatabase: 	- ListPositionContext.cs inherited from DbContext to define DbSet and DbContextOptions 	- ListPosition.cs - model of single database list position 	- entry in Startup.cs - Added InMemoryDatabase service in middleware pipeline configuration 2. Added page TODO/Index with logic to display and create list positions

---
+++ TodoList/Data/ListPositionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class ListPositionContext : DbContext
+    {
+        public ListPositionContext(DbContextOptions<ListPositionContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ListPosition> ListPositions { get; set; }
+    }
+}
---
+++ TodoList/Pages/TODO/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+using TodoList.Models;
+
+namespace TodoList.Pages.TODO
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ListPositionContext _context;
+        public IndexModel(ListPositionContext context)
+        {
+            _context = context;
+        }
+        public IList<ListPosition> listPositions { get; set; }
+        [BindProperty]
+        public ListPosition listPosition { get; set; }
+        public async Task OnGetAsync()
+        {
+            listPositions = await _context.ListPositions.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            listPosition.WriteStamp = DateTime.Now;
+
+            _context.ListPositions.Add(listPosition);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
